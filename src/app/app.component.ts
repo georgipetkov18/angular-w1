eb39a6fc75423ee96ebc6ab77e6a75a9801aa0f7
Georgi Petkov <george18petkov@gmail.com>
2024-05-17 12:19:21 +0300

    Finalize work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TrafficLightComponent } from './traffic-light/traffic-light.component';
+import { Subject, Subscription } from 'rxjs';
+import { EmergencyService } from './emergency.service';
 
 @Component({
   selector: 'app-root',
@@ -9,14 +11,29 @@ import { TrafficLightComponent } from './traffic-light/traffic-light.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy{
   public horizontalLight: 'r' | 'g' | 'y' = 'r';
   public verticalLight: 'r' | 'g' | 'y' = 'g';
+  public emergencyBtnIsDisabled = false;
+  public emergencyService: EmergencyService = inject(EmergencyService);
   
   private lastHorizontalMainLight: 'r' | 'g' | 'y' = 'g';
   private lastVerticalMainLight: 'r' | 'g' | 'y' = 'r';
+  private currentTimeout!: any;
+  private emergencyModeEndSub!: Subscription;
+
 
   ngOnInit(): void {
+    this.emergencyModeEndSub = this.emergencyService.emergencyModeEnd.subscribe(() => {
+      if (!this.emergencyBtnIsDisabled) {
+        this.switchToMainLights();
+        this.emergencyBtnIsDisabled = true;
+        setTimeout(() => {
+          this.emergencyBtnIsDisabled = false;
+        }, 10000);
+      }
+    });
+
     this.switchToMainLights();
   };
 
@@ -26,19 +43,28 @@ export class AppComponent implements OnInit {
 
     this.lastHorizontalMainLight = this.horizontalLight;
     this.lastVerticalMainLight = this.verticalLight;
-    setTimeout(() => {
+    this.currentTimeout = setTimeout(() => {
       this.switchToYellowLights();
     }, 5000);
   }
 
   switchToYellowLights() {
-    debugger
     this.horizontalLight = 'y';
     this.verticalLight = 'y';
 
-    setTimeout(() => {
+    this.currentTimeout = setTimeout(() => {
       this.switchToMainLights();
     }, 2000);
+  }
 
+  onEmergencyBtnClick() {
+    clearTimeout(this.currentTimeout);
+    this.horizontalLight = 'y';
+    this.verticalLight = 'y';
+    this.emergencyService.startEmergencyMode();
+  }
+
+  ngOnDestroy(): void {
+    this.emergencyModeEndSub.unsubscribe();
   }
 }
--- src/app/traffic-light/traffic-light.component.ts
+++ src/app/traffic-light/traffic-light.component.ts
@@ -1,4 +1,6 @@
-import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnDestroy, OnInit, Output, SimpleChanges, inject } from '@angular/core';
+import { Subject, Subscription } from 'rxjs';
+import { EmergencyService } from '../emergency.service';
 
 @Component({
   selector: 'app-traffic-light',
@@ -7,12 +9,43 @@ import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
   templateUrl: './traffic-light.component.html',
   styleUrl: './traffic-light.component.css'
 })
-export class TrafficLightComponent implements OnChanges{
-  @Input({required: true}) public isHorizontal!: boolean;
+export class TrafficLightComponent implements OnInit, OnChanges, OnDestroy {
+  @Input({ required: true }) public isHorizontal!: boolean;
+  @Input({ required: true }) public light!: 'r' | 'g' | 'y' | null;
   @Input() public reversed: boolean = false;
-  @Input({required: true}) public light!: 'r' | 'g' | 'y';
+  public emergencyService: EmergencyService = inject(EmergencyService);
+  private emergencyModeStartSub!: Subscription;
+
+  ngOnInit(): void {
+    this.emergencyService.emergencyModeStart.subscribe(() => {
+      this.executeEmergencyMode();
+    })
+  }
 
   ngOnChanges(changes: SimpleChanges): void {
     this.light = changes['light'].currentValue;
+  }
+
+  onCrossBtnClicked() {
+    if (this.light === 'y') {
+      alert('Неправилно пресичане');
+    }
+  }
+
+  executeEmergencyMode() {
+    let interval: any;
+
+    setTimeout(() => {
+      clearInterval(interval);
+      this.emergencyService.endEmergencyMode();
+    }, 10000);
+
+    interval = setInterval(() => {
+      this.light ? this.light = null : this.light = 'y';
+    }, 1000);
+  }
+
+  ngOnDestroy(): void {
+    this.emergencyModeStartSub.unsubscribe();
   }
 }
